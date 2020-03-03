80a101701881cd7b97a8c684c7d6c3f229ab2999
kanchaojie <kanchaojie@126.com>
2020-03-03 17:45:37 +0800

    添加相关配置

--- springbootdemo/src/main/java/com/kcj/Application.java
+++ springbootdemo/src/main/java/com/kcj/Application.java
@@ -3,8 +3,12 @@ package com.kcj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Application {
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
