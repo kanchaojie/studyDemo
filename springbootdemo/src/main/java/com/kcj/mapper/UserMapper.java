325d44d9614a17a04c7f41bd000d4dcaee8de716
kanchaojie <kanchaojie@126.com>
2020-03-05 16:25:27 +0800

    解决service中自动注入mapper报错的问题（不解决也不影响使用）

--- springbootdemo/src/main/java/com/kcj/mapper/UserMapper.java
+++ springbootdemo/src/main/java/com/kcj/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.kcj.mapper;
 
 import com.kcj.pojo.User;
+import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+@Component("userMapper")
 public interface UserMapper extends Mapper<User> {
 }
